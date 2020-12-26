c4ad29a2bd9a7bc8d8589a7bd396fd3e169bb669
sontx <xuanson33bk@gmail.com>
2020-12-26 19:48:01 +0700

    - Adjust about ui

--- Views/AboutWindow.xaml.cs
+++ Views/AboutWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System.Diagnostics;
 using System.Windows;
+using System.Windows.Input;
 using System.Windows.Navigation;
 
 namespace DropZone.Views
@@ -14,10 +15,24 @@ namespace DropZone.Views
             InitializeComponent();
         }
 
+        protected override void OnMouseDoubleClick(MouseButtonEventArgs e)
+        {
+            base.OnMouseDoubleClick(e);
+            Close();
+        }
+
+        protected override void OnMouseDown(MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+                this.DragMove();
+            base.OnMouseDown(e);
+        }
+
         private void link_OnRequestNavigate(object sender, RequestNavigateEventArgs e)
         {
             Process.Start(new ProcessStartInfo(e.Uri.AbsoluteUri));
             e.Handled = true;
+            Close();
         }
     }
 }
