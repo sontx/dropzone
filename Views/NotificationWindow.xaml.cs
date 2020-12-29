f0b7db638955b7d4a006fd09c7a772d41fa3813b
sontx <xuanson33bk@gmail.com>
2020-12-29 16:32:59 +0700

    * Fix notification window shows wrong position

--- Views/NotificationWindow.xaml.cs
+++ Views/NotificationWindow.xaml.cs
@@ -1,12 +1,10 @@
 ﻿using DropZone.Utils;
 using DropZone.ViewModels;
-using System;
 using System.IO;
 using System.Media;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Input;
-using System.Windows.Threading;
 
 namespace DropZone.Views
 {
@@ -18,16 +16,6 @@ namespace DropZone.Views
         public NotificationWindow()
         {
             InitializeComponent();
-
-            Dispatcher.BeginInvoke(DispatcherPriority.ApplicationIdle, new Action(() =>
-            {
-                var workingArea = System.Windows.Forms.Screen.PrimaryScreen.WorkingArea;
-                var transform = PresentationSource.FromVisual(this).CompositionTarget.TransformFromDevice;
-                var corner = transform.Transform(new Point(workingArea.Right, workingArea.Bottom));
-
-                this.Left = corner.X - this.ActualWidth;
-                this.Top = corner.Y - this.ActualHeight;
-            }));
 
             Loaded += NotificationWindow_Loaded;
             MouseLeftButtonDown += NotificationWindow_MouseLeftButtonDown;
@@ -44,6 +32,12 @@ namespace DropZone.Views
 
         private void NotificationWindow_Loaded(object sender, RoutedEventArgs e)
         {
+            var workingArea = System.Windows.Forms.Screen.PrimaryScreen.WorkingArea;
+            var transform = PresentationSource.FromVisual(this).CompositionTarget.TransformFromDevice;
+            var corner = transform.Transform(new Point(workingArea.Right, workingArea.Bottom));
+
+            this.Left = corner.X - this.ActualWidth;
+            this.Top = corner.Y - this.ActualHeight;
             Loaded -= NotificationWindow_Loaded;
             PlayNotificationSound();
             ScheduleClose();
